17eef88e93ce4d3bb9523be77f52083f3c600e31
Iyan Theekshana <iyan.dev123@gmail.com>
2023-01-11 10:23:55 +0100

    updated readme file

---
+++ src/app/person/person.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css'],
+})
+export class PersonComponent {
+  username = '';
+
+  onClickRemoveText() {
+    this.username = '';
+  }
+}
